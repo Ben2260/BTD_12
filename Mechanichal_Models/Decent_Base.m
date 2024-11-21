50095a62d0485ac213e006d363ec75c65c2bb42f
cadejmacklin25 <cama4854@colorado.edu>
2024-11-21 16:08:05 -0700

    Add files via upload

--- Mechanichal_Models/Decent_Base.m
+++ Mechanichal_Models/Decent_Base.m
@@ -5,7 +5,7 @@ warning('off')
 Z = 3.546746135626791e+04; % Taken from acent code
 %%Ascent
 init_cond = [0,0,Z,0,0,0];
-[t,y] = ode45(@(t,y) flightpath_ode(t,y), [0 500], init_cond);
+[t,y] = ode45(@(t,y) flightpath_ode(t,y), [0 4000], init_cond);
 % Units convert:
 height_feet = y(:,3)./0.3048;
 
@@ -15,15 +15,16 @@ height_feet = y(:,3)./0.3048;
 
 figure(1)
 plot(t,height_feet)
-xlabel("we")
 hold on
 plot(t(end),height_feet(end), 'marker','x', 'markersize',10,'linewidth',2)
+xlabel("time")
+ylabel('position [ft]')
 hold off
 function [dydt] = flightpath_ode(t,y)
 
     % Constants:
-    m_tot = 3.5 + 2.2+ 0.5 + 0.15 +2; %freshman payloads + our beacon + parachute + tether + 2000g balloon [kg];
-    cd = .47; %estimated coefficient of drag of a weather balloon from google
+    m_tot = 3.5 + 2.2+ 0.5 + 0.15; %freshman payloads + our beacon + parachute + tether + 2000g balloon [kg];
+    cd = .97; %estimated coefficient of drag of a weather balloon from google
     R_earth = 6371*1000; %radius of earth in boulder
     GM = (6.6743*10^-11)*(5.97218*10^24);
 
@@ -42,16 +43,16 @@ function [dydt] = flightpath_ode(t,y)
                                      % we nolonger need anything but the air
                                      % density at those heights
         
-         g = GM./(R_earth+Z).^2; %gravity with respect to altitude  
+         g = 9.81*(1-(2*Z)/R_earth); %gravity with respect to altitude  
         
         % Forces
         weight_force =(m_tot)*g;
-        area = 0.20828*0.09652; % Estimated size of largest face (review)
+        area = 2.98; % [m^2]
 
-        %% HEY CADE CHECK THIS DRAG EQUATION AND CHANGE IT FROM HERE DOWN
+     
         Drag = 0.5*cd*vZ.^2.*rho*area; 
     
-        F_net =  - weight_force + Drag;
+        F_net = Drag- weight_force;
         ascent_accel = F_net/(m_tot); %acceleration of total system
     
         ax = 0;
