ae7e8e71b01bbe44cd804580a12a04bfec225ab2
Ben Reynolds <90279857+Ben2260@users.noreply.github.com>
2024-11-20 20:51:40 -0700

    Create Decent_Base.m
    To be updated this code NEEDS:
    - Corrected area of the beacon
    - Effects of parashewt on drag

---
+++ Mechanichal_Models/Decent_Base.m
@@ -0,0 +1,64 @@
+clear
+close all
+clc
+warning('off')
+Z = 3.546746135626791e+04; % Taken from acent code
+%%Ascent
+init_cond = [0,0,Z,0,0,0];
+[t,y] = ode45(@(t,y) flightpath_ode(t,y), [0 500], init_cond);
+% Units convert:
+height_feet = y(:,3)./0.3048;
+
+%%Descent
+
+%%Freefall
+
+figure(1)
+plot(t,height_feet)
+xlabel("we")
+hold on
+plot(t(end),height_feet(end), 'marker','x', 'markersize',10,'linewidth',2)
+hold off
+function [dydt] = flightpath_ode(t,y)
+
+    % Constants:
+    m_tot = 3.5 + 2.2+ 0.5 + 0.15 +2; %freshman payloads + our beacon + parachute + tether + 2000g balloon [kg];
+    cd = .47; %estimated coefficient of drag of a weather balloon from google
+    R_earth = 6371*1000; %radius of earth in boulder
+    GM = (6.6743*10^-11)*(5.97218*10^24);
+
+    
+    
+    % Unpack the variables
+    X = y(1); % X position
+    Y = y(2); % Y position
+    Z = y(3); % Z position (height)
+    vX = y(4); % X velocity
+    vY = y(5); % Y velocity
+    vZ = y(6); % Z velocity
+    if round(Z,-3) ~= 0
+        
+        [~,~,~,rho] = atmoscoesa(Z); % standard atmosphere for our altitudes
+                                     % we nolonger need anything but the air
+                                     % density at those heights
+        
+         g = GM./(R_earth+Z).^2; %gravity with respect to altitude  
+        
+        % Forces
+        weight_force =(m_tot)*g;
+        area = 0.20828*0.09652; % Estimated size of largest face (review)
+
+        %% HEY CADE CHECK THIS DRAG EQUATION AND CHANGE IT FROM HERE DOWN
+        Drag = 0.5*cd*vZ.^2.*rho*area; 
+    
+        F_net =  - weight_force + Drag;
+        ascent_accel = F_net/(m_tot); %acceleration of total system
+    
+        ax = 0;
+        ay = 0;
+        az = ascent_accel;
+        dydt = [vX; vY; vZ; ax; ay; az];
+    else
+        dydt = [0;0;0;0;0;0];
+    end
+end
